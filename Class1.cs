0c1c2c6fc88f8b58e79cb7a4854564396fd0a459
Muthurisha28 <rishasubbiah@gmail.com>
2021-12-29 14:23:45 +0530

    file added

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handson_28_12
+{
+    class Class1
+    {
+        public int a0, b0;
+        public int GetSecondLargest(int[] a)
+        {
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > a0)
+                {
+                    b0 = a0;
+                    a0 = a[i];
+                }
+                else if (a[i] > b0) b0 = a[i];
+            }
+            return b0;
+
+
+        }
+
+    }
+
+}
+
